a151f8f60e3ad43e8731dc29e5ba4274c194b5bc
NormanMorman <normanrichardson@gmail.com>
2021-06-03 00:45:39 -0700

    Minor corrections to GlassTypes
    * pint units
    * load_duration_factor
    * scipy imports

--- GlassTypes.py
+++ GlassTypes.py
@@ -1,10 +1,9 @@
 import abc
 import FourSidedGlassCalc as fsgc
-import scipy
+import scipy.stats as ss
 
 class GlassType:
     __metaclass__ = abc.ABCMeta
-    
     def __init__(self, stress_surface, stress_edge, duration_factor, surf_factors, coef_variation = 0.2):
         self.stress_surface = stress_surface
         self.stress_edge = stress_edge
@@ -14,19 +13,18 @@ class GlassType:
     
     @fsgc.ureg.check(None, '[time]')
     def load_duration_factor(self, time = 3*fsgc.ureg.sec):
-        return 1 / ((time.to(fsgc.ureg.sec).magnitude/3)**self.duration_factor)
+        return 1 / ((time.to(fsgc.ureg.sec).magnitude/3)**(1/self.duration_factor))
     def design_factor(self, ratio):
-        return 1 / ( 1 - self.coef_variation*scipy.norm.ppf(1-ratio) )
+        return 1 / ( 1 - self.coef_variation*ss.norm.ppf(1-ratio) )
     def prob_breakage_factor(self, ratio):
         return self.design_factor(0.008) / self.design_factor(ratio)
     def surf_treat_factor(self, surf_treat):
         return self.surf_factors[surf_treat]
-    
         
 class Annealed(GlassType):
     def __init__(self):
-        stress_surface = 23.3 * fsgc*MPa
-        stress_edge    = 18.3 * fsgc*MPa
+        stress_surface = 23.3 * fsgc.ureg.MPa
+        stress_edge    = 18.3 * fsgc.ureg.MPa
         duration_factor = 16
         coef_variation = 0.22
         surf_factors = {
@@ -39,8 +37,8 @@ class Annealed(GlassType):
 
 class HeatStrengthened(GlassType):
     def __init__(self):
-        stress_surface = 46.6 * fsgc*MPa
-        stress_edge    = 36.5 * fsgc*MPa
+        stress_surface = 46.6 * fsgc.ureg.MPa
+        stress_edge    = 36.5 * fsgc.ureg.MPa
         duration_factor = 31.7
         coef_variation = 0.15
         surf_factors = {
@@ -49,12 +47,12 @@ class HeatStrengthened(GlassType):
             "Acid etching" : 0.5,
             "Sandblasting" : 0.5
         }
-        super(Annealed, self).__init__(stress_surface, stress_edge, duration_factor, surf_factors, coef_variation)
+        super(HeatStrengthened, self).__init__(stress_surface, stress_edge, duration_factor, surf_factors, coef_variation)
 
 class FullyTempered(GlassType):
     def __init__(self):
-        stress_surface = 93.1 * fsgc*MPa
-        stress_edge    = 73.0 * fsgc*MPa
+        stress_surface = 93.1 * fsgc.ureg.MPa
+        stress_edge    = 73.0 * fsgc.ureg.MPa
         duration_factor = 47.5
         coef_variation = 0.1
         surf_factors = {
@@ -63,4 +61,4 @@ class FullyTempered(GlassType):
             "Acid etching" : 0.5,
             "Sandblasting" : 0.5
         }
-        super(Annealed, self).__init__(stress_surface, stress_edge, duration_factor, surf_factors, coef_variation)
+        super(FullyTempered, self).__init__(stress_surface, stress_edge, duration_factor, surf_factors, coef_variation)
