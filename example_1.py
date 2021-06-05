e956208938699028a8a6127d2a182135f9879094
NormanMorman <normanrichardson@gmail.com>
2021-06-05 03:39:38 -0700

    Update imports in examples to use packages

--- example_1.py
+++ example_1.py
@@ -1,19 +1,21 @@
 
 # Example from NCSEA Engineering Structural Glass Design Guide chapter 8 example 5
 
-from typing import overload
-import FourSidedGlassCalc as fsgc
-import GlassTypes as gt
+from structuralglass import ureg, Q_
+import structuralglass.glass_types as gt
+import structuralglass.materials as mat
+import structuralglass.equiv_thick_models as et
+import structuralglass.demands as dem
 
 # Plate dimensions
-a = 4000 * fsgc.ureg.mm
-b = 2000 * fsgc.ureg.mm
-t1nom = 6*fsgc.ureg.mm
-t2nom = 6*fsgc.ureg.mm
-t3nom = 6*fsgc.ureg.mm
+a = 4000 * ureg.mm
+b = 2000 * ureg.mm
+t1nom = 6 * ureg.mm
+t2nom = 6 * ureg.mm
+t3nom = 6 * ureg.mm
 
 # Panel force
-windLoad = 1.436*fsgc.ureg.kPa
+windLoad = 1.436 * ureg.kPa
 
 # Allowable stress
 ft = gt.FullyTempered()
@@ -23,24 +25,24 @@ ft.coef_variation = 0.2
 # used the HS 10 year load duration factor. Here the FT 3s load duration factor 
 # is used.
 allow_stress = ft.prob_breakage_factor(1/1000) \
-    * ft.load_duration_factor(3 *fsgc.ureg.sec) \
+    * ft.load_duration_factor(3 *ureg.sec) \
     * ft.surf_treat_factor("None") * ft.stress_edge
 
 # Allowable deflection
 defl_max = min(a,b)/75
 
 # Plys
-ply1 = fsgc.GlassPly.from_nominal_thickness(t1nom,"FT")
-ply2 = fsgc.GlassPly.from_nominal_thickness(t2nom,"FT")
-ply3 = fsgc.GlassPly.from_nominal_thickness(t3nom,"FT")
+ply1 = mat.GlassPly.from_nominal_thickness(t1nom,"FT")
+ply2 = mat.GlassPly.from_nominal_thickness(t2nom,"FT")
+ply3 = mat.GlassPly.from_nominal_thickness(t3nom,"FT")
 
 # Package specifying the model type
-package1 = fsgc.MonolithicMethod([ply1, ply2])
-package2 = fsgc.MonolithicMethod([ply3])
+package1 = et.MonolithicMethod([ply1, ply2])
+package2 = et.MonolithicMethod([ply3])
 buildup = [package1, package2]
 
 # Panel
-panel = fsgc.GlassPanel(a , b, buildup, windLoad)
+panel = dem.GlassPanel(a , b, buildup, windLoad)
 
 # Results
 print("-------------Package values-------------")
@@ -57,5 +59,5 @@ print("Stress in ply 3: {:.2f~P} => {:.2%~P}".format(panel.stress[ply3], panel.s
 print("-------------Displacement values of the package-------------")
 print("Allowable deflection: {:.2f~P}".format(defl_max))
 print("Deflection of package 1: {:.2f~P} => {:.2%~P}".format(panel.deflection[package1], panel.deflection[package1]/defl_max))
-print("Deflection of package 2: {:.2f~P} => {:.2%~P}".format(panel.deflection[package2].to(fsgc.ureg.mm), panel.deflection[package1]/defl_max))
+print("Deflection of package 2: {:.2f~P} => {:.2%~P}".format(panel.deflection[package2].to(ureg.mm), panel.deflection[package1]/defl_max))
 
--- example_2.py
+++ example_2.py
@@ -1,26 +1,28 @@
 
 # Example from NCSEA Engineering Structural Glass Design Guide chapter 8 example 3
 
-import FourSidedGlassCalc as fsgc
+from structuralglass import ureg, Q_
+import structuralglass.materials as mat
+import structuralglass.equiv_thick_models as et
 
 # Plate dimensions
-a = 1000 * fsgc.ureg.mm
-t1nom = 12*fsgc.ureg.mm
-t2nom = 12*fsgc.ureg.mm
+a = 1000 * ureg.mm
+t1nom = 12*ureg.mm
+t2nom = 12*ureg.mm
 
 # Interlayer PVB at 30degC for 1 day load duration
-G_pvb = 0.281*fsgc.ureg.MPa
-t_pvb = 0.89*fsgc.ureg.mm
-interlayer = fsgc.InterLayer(t_pvb, G_pvb)
+G_pvb = 0.281*ureg.MPa
+t_pvb = 0.89*ureg.mm
+interlayer = mat.InterLayer(t_pvb, G_pvb)
 
 # Plys
-ply1 = fsgc.GlassPly.from_nominal_thickness(t1nom,"FT")
-ply2 = fsgc.GlassPly.from_nominal_thickness(t2nom,"FT")
+ply1 = mat.GlassPly.from_nominal_thickness(t1nom,"FT")
+ply2 = mat.GlassPly.from_nominal_thickness(t2nom,"FT")
 
 # Package specifying the model type
-package = fsgc.ShearTransferCoefMethod([ply1,interlayer, ply2],a)
+package = et.ShearTransferCoefMethod([ply1,interlayer, ply2],a)
 
 print("-------------Package values-------------")
-print("Effective displacement thickness: {:.3f~P} ({:.3f~P})".format(package.h_efw, package.h_efw.to(fsgc.ureg.inch)))
-print("Effective stress thickness of the package with reference to ply1: {:.3f~P} ({:.3f~P})".format(package.h_efs[ply1], package.h_efs[ply1].to(fsgc.ureg.inch)))
-print("Effective stress thickness of the package with reference to ply2: {:.3f~P} ({:.3f~P})".format(package.h_efs[ply2], package.h_efs[ply2].to(fsgc.ureg.inch)))
+print("Effective displacement thickness: {:.3f~P} ({:.3f~P})".format(package.h_efw, package.h_efw.to(ureg.inch)))
+print("Effective stress thickness of the package with reference to ply1: {:.3f~P} ({:.3f~P})".format(package.h_efs[ply1], package.h_efs[ply1].to(ureg.inch)))
+print("Effective stress thickness of the package with reference to ply2: {:.3f~P} ({:.3f~P})".format(package.h_efs[ply2], package.h_efs[ply2].to(ureg.inch)))
