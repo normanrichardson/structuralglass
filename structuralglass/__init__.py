a0408623a9179d2938d16551e18b675959aa4f0a
NormanMorman <normanrichardson@gmail.com>
2021-06-08 06:31:48 -0700

    Added a units file as a package resource
    This will hopefully be a place to extend structural units like force_per_unit_length etc.

--- structuralglass/__init__.py
+++ structuralglass/__init__.py
@@ -1,7 +1,15 @@
 import pint
-# Create the default pint unit registry
+from . import resources
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+# Load the file stream for the units file
+unit_file = pkg_resources.open_text(resources, 'unit_def.txt')
+
+# Setup pint for the package
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
-
-# Add psf
-ureg.define('pound_force_per_square_foot = pound * gravity / foot ** 2 = psf')
+ureg.load_definitions(unit_file)
