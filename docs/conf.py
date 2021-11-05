ff2bc8c30a34322eccecbe05fa7a95b00decbb90
Norman JN Richardson <normanrichardson@gmail.com>
2021-11-05 14:06:13 -0700

    Configure Intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,3 +55,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "pint": ("https://pint.readthedocs.io/en/latest/", None),
+    "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
+}
